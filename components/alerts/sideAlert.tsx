b1d4d4f5df91d164d2140efab562d0df55369c60
safal <safal.sharma.ss@gmail.com>
2022-06-29 07:06:37 +0545

    design changes in alert component.

--- components/alerts/sideAlert.tsx
+++ components/alerts/sideAlert.tsx
@@ -2,6 +2,7 @@ import React, {Dispatch, useEffect, useState} from "react";
 import {ALERT_TYPE_DANGER, ALERT_TYPE_SUCCESS, ALERT_TYPE_WARNING, ALERT_TYPES} from "../../constants";
 import {Alert} from "../../@types/alert";
 import {CheckCircleOutline, EmojiSadOutline, InformationCircleOutline} from "heroicons-react";
+import PrimaryButton from "../buttons/primaryButton";
 
 
 const SideAlert = (props: { alert: Alert }) => {
@@ -37,42 +38,48 @@ const SideAlert = (props: { alert: Alert }) => {
                 <div id="alert"
                      className={`transition duration-500 ease-in-out w-11/12 mx-auto py-3 px-4 dark:bg-gray-800 bg-white md:flex items-center justify-between shadow rounded ${flag ? 'translate-show' : 'translate-hide'} `}>
                     <div className="sm:flex sm:items-start lg:items-center">
-                        <div className="flex items-center gap-x-2">
-                            <div
-                                className={`mr-2 ${alertType?.type == 0 ? "text-lime-500" : alertType?.type == 1 ? "text-yellow-400" : alertType?.type == 2 ? "text-red-500" : "text-white"}`}>
-
-                                {alertType?.type == ALERT_TYPE_SUCCESS && <>
-                                    <CheckCircleOutline className={"w-6 h-6"} />
-                                </>
-                                }
-
-                                {alertType?.type == ALERT_TYPE_WARNING && <>
-                                    <InformationCircleOutline className={"w-6 h-6"} />
-                                </>
-                                }
-                                {alertType?.type == ALERT_TYPE_DANGER && <>
-                                    <EmojiSadOutline className={"w-6 h-6"} />
-                                </>
-                                }
-
-                            </div>
-                            <p className="mr-2  text-base font-bold text-gray-800 dark:text-gray-100">{props.alert.title}</p>
-                        </div>
-                        {props.alert.message &&
-                            <>
+                        <div className="flex gap-x-4  items-center">
+                            <div>
                                 <div
-                                    className="h-1 w-1 bg-gray-300 dark:bg-gray-700 rounded-full mr-2 hidden xl:block"/>
-                                <p className="text-sm sm:text-base text-gray-600 dark:text-gray-400 pt-2 sm:pt-0 pb-2 sm:pb-0">{props.alert.message}</p>
-                            </>
-                        }
+                                    className={`mr-2 ${alertType?.type == 0 ? "text-lime-500" : alertType?.type == 1 ? "text-yellow-400" : alertType?.type == 2 ? "text-red-500" : "text-white"}`}>
+
+                                    {alertType?.type == ALERT_TYPE_SUCCESS && <>
+                                        <CheckCircleOutline className={"w-6 h-6"}/>
+                                    </>
+                                    }
+
+                                    {alertType?.type == ALERT_TYPE_WARNING && <>
+                                        <InformationCircleOutline className={"w-6 h-6"}/>
+                                    </>
+                                    }
+                                    {alertType?.type == ALERT_TYPE_DANGER && <>
+                                        <EmojiSadOutline className={"w-6 h-6"}/>
+                                    </>
+                                    }
+                                </div>
+                            </div>
+                            <div className={"flex flex-col"}>
+                            <p className=" text-base font-bold text-gray-800 dark:text-gray-100 leading-9">{props.alert.title}</p>
+
+                            {props.alert.message &&
+                                <>
+                                    <p className="text-sm sm:text-base text-gray-600 dark:text-gray-400 pt-2 sm:pt-0 pb-2 sm:pb-0">{props.alert.message}</p>
+                                </>
+                            }
+                            </div>
+                        </div>
+
                     </div>
+
                     <div className="flex items-center justify-end sm:mt-4 md:mt-0">
                         {props.alert.action &&
                             <>
-                                <button
+
+                                <PrimaryButton
+                                    name={props.alert.actionButtonText || 'Action'}
                                     onClick={props.alert.action}
-                                    className="ml-4 focus:outline-none mr-8 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-6 py-2 text-xs">{props.alert.actionButtonText}
-                                </button>
+                                    class={"text-sm mx-4"}
+                                />
                             </>
                         }
 
