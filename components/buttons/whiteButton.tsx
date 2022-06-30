5f98d0a861694a8d62ae44c27f3976853858719f
safal <safal.sharma.ss@gmail.com>
2022-06-30 10:30:14 +0545

    design improvements in `job post` list.

--- components/buttons/whiteButton.tsx
+++ components/buttons/whiteButton.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {overrideTailwindClasses} from "tailwind-override";
 
-function WhiteButton(props: { name: string, class?: string, onClick?: () => void }) {
+function WhiteButton(props: { name: string, class?: string, onClick?: () => void, leftChildren?: React.ReactNode }) {
     return (
         <button
             type={"button"}
             onClick={props.onClick}
-            className={overrideTailwindClasses(`mx-2 my-2 bg-white transition duration-150 ease-in-out focus:outline-none hover:bg-gray-100 rounded text-indigo-700 px-6 py-2 text-xs ${props.class}`)}>{props.name}</button>
+            className={overrideTailwindClasses(`flex items-center mx-2 my-2 bg-white transition duration-150 ease-in-out focus:outline-none hover:bg-gray-100 rounded text-indigo-700 px-6 py-2 text-xs ${props.class}`)}
+        >
+            {props.leftChildren}
+            {props.name}
+        </button>
     );
 }
 
--- components/post/PostListItem.tsx
+++ components/post/PostListItem.tsx
@@ -5,6 +5,9 @@ import Link from "next/link"
 import Image from "next/image";
 import WhiteButton from "../buttons/whiteButton";
 import {BASE_URL, IMAGE_COMPANY_PLACEHOLDER} from "../../others/config";
+import {CurrencyRupeeIcon, EyeIcon, LocationMarkerIcon} from "@heroicons/react/solid";
+import { formatCurrency } from '../../others/helpers';
+import {ClockIcon} from "@heroicons/react/outline";
 
 function PostListItem(props: {
     post: Post
@@ -17,7 +20,7 @@ function PostListItem(props: {
         <div>
             <div
                 className="flex flex-col md:mx-auto rounded bg-white overflow-hidden">
-                <div className={'relative h-56 w-full rounded-t bg-gray-200'}>
+                <div className={'relative h-56 w-full rounded-t bg-gray-200 overflow-hidden'}>
                     <Image
                         onError={(e)=>{
                             console.log("error:")
@@ -40,16 +43,19 @@ function PostListItem(props: {
                 </div>
                 <div className="w-full">
 
-                    <div className="pt-6 p-4">
+                    <div className="p-4 pb-2">
                         <div className="flex items-center">
                             <h2 className="text-lg font-semibold truncate">{post.title}</h2>
                             {moment(post.created_date).isValid() &&
-                                <p className="text-xs text-gray-600 pl-5 ml-auto text-right">{moment(post.created_date).fromNow()}</p>
+                                <p className="text-xs text-gray-600 pl-5 ml-auto text-right">
+                                    <ClockIcon className={'w-4 h-4 ml-auto'} />
+                                    {moment(post.created_date).fromNow()}
+                                </p>
                             }
                         </div>
                         {/*<p className="text-xs text-gray-600 mt-3">{post.description}</p>*/}
 
-                        <div className="flex mt-4">
+                        <div className="flex mt-2">
                             <div className={" px-2 bg-gray-200 rounded py-1"}>
                                 {/*<p className="text-xs text-gray-600 px-2 bg-gray-200 py-1">{post.field?.name}</p>*/}
                                 <p className="text-xs text-gray-600 truncate ">{post.position}</p>
@@ -60,21 +66,27 @@ function PostListItem(props: {
                                 </div>
                             </Link>
                         </div>
-                        <div className="flex flex-col md:flex-row gap-2 md:items-center justify-between py-4">
-                            <h2 className="text-indigo-700 text-xs font-semibold truncate">{post.location}</h2>
+                        <div className="flex flex-col md:flex-row gap-2 md:items-center py-2 text-indigo-700 text-xs">
+                            <LocationMarkerIcon className={'w-4 h-4'} />
+                            <h2 className="font-semibold truncate">{post.location}</h2>
 
                         </div>
                     </div>
-                    <div className={"pl-4 p-2 flex items-center justify-between"}>
-                        <h3 className="text-indigo-600 text-sm font-normal truncate ">
-                            {/*<span className="text-xs text-indigo-800"></span>*/}
-                            Rs. {post.salary_low} - Rs. {post.salary_high}</h3>
-                        {/*TODO: No id from API */}
-                        <Link href={"/post/20"}>
+                    <div className={"pl-4 p-2 flex items-center gap-1 text-indigo-600 text-sm"}>
+                        <CurrencyRupeeIcon className={"w-4 h-4"} />
+                        <h3 className="font-normal truncate">
+                            {formatCurrency(post.salary_low)} - {formatCurrency(post.salary_high)}
+                        </h3>
+                        <div className={"ml-auto"}>
+                        <Link href={`/post/${post.id}`}>
                             <a>
-                                <WhiteButton name={"View"} class={"bg-indigo-50 text-sm font-medium"}/>
+                                <WhiteButton
+                                    name={"View"}
+                                    class={"bg-indigo-50 border border-indigo-300 text-sm font-medium"}
+                                />
                             </a>
                         </Link>
+                        </div>
                     </div>
                 </div>
             </div>
