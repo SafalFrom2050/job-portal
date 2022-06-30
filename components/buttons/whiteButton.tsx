2157bd5479a0108173f0830047bc2c119f6e2262
safal <safal.sharma.ss@gmail.com>
2022-07-01 02:55:50 +0545

    style changes.

--- components/buttons/whiteButton.tsx
+++ components/buttons/whiteButton.tsx
@@ -6,7 +6,7 @@ function WhiteButton(props: { name: string, class?: string, onClick?: () => void
         <button
             type={"button"}
             onClick={props.onClick}
-            className={overrideTailwindClasses(`flex items-center mx-2 my-2 bg-white transition duration-150 ease-in-out focus:outline-none hover:bg-gray-100 rounded text-indigo-700 px-6 py-2 text-xs ${props.class}`)}
+            className={overrideTailwindClasses(`flex items-center justify-center mx-2 my-2 bg-white transition duration-150 ease-in-out focus:outline-none hover:bg-gray-100 rounded text-indigo-700 px-6 py-2 text-xs ${props.class}`)}
         >
             {props.leftChildren}
             {props.name}
