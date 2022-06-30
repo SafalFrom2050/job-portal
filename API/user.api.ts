d7338d5b7e58e4216ec7fbc9e15d436e058463cb
safal <safal.sharma.ss@gmail.com>
2022-07-01 02:57:10 +0545

    make user profile page dynamic.

--- API/user.api.ts
+++ API/user.api.ts
@@ -144,13 +144,38 @@ export const updateUser = async (axiosInstance: AxiosInstance, user: User | Upda
 }
 
 
-export const getUserById = async (axiosInstance: AxiosInstance | null, id: number) => {
+export const getUserById = async (axiosInstance: AxiosInstance | null, id: string) => {
 
     if (axiosInstance == null) return {data: [], status: null} as Response
 
     try {
         const response = await axiosInstance.get(
             `user/${id}/`
+        );
+
+        console.log(response)
+        const {data, status} = response
+
+        console.log(JSON.stringify(data));
+
+        return {data, status} as Response;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const e = error as AxiosError
+            return {data:e.response?.data, status: e.response?.status};
+        } else {
+            return {data: [], status: null} as Response;
+        }
+    }
+}
+
+export const getUsers = async (axiosInstance: AxiosInstance | null) => {
+
+    if (axiosInstance == null) return {data: [], status: null} as Response
+
+    try {
+        const response = await axiosInstance.get(
+            `user/`
         );
 
         console.log(response)
