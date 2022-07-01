c3fe26278574fa212a000a4d28bb3dac3fa45da0
safal <safal.sharma.ss@gmail.com>
2022-07-01 06:30:36 +0545

    add error handling.

--- API/auth.api.ts
+++ API/auth.api.ts
@@ -1,6 +1,25 @@
-import {AxiosInstance} from "axios";
+import axios, {AxiosError, AxiosInstance} from "axios";
+import {Response} from "./http-common";
 
 
-export function getCurrentUser(axiosInstance: AxiosInstance) {
-    return axiosInstance?.get('user/auth/get_profile/');
+export async function getCurrentUser(axiosInstance: AxiosInstance | null) {
+
+    if (axiosInstance == null) return {data: [], status: null} as Response
+
+    try {
+        const response = await axiosInstance.get(
+            `user/auth/get_profile/`
+        );
+
+        const {data, status} = response
+
+        return {data, status} as Response;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const e = error as AxiosError
+            return {data:e.response?.data, status: e.response?.status};
+        } else {
+            return {data: [], status: null} as Response;
+        }
+    }
 }
