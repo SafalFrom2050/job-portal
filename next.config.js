41e4028a46d982d5380e2b95b656ec743947ac74
safal <safal.sharma.ss@gmail.com>
2022-07-01 08:01:53 +0545

    add configurations required for `PWA` support.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa");
+
+const pwaConfig = withPWA({
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true
+    }
+})
+
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['job-post-test.herokuapp.com', 'slcsolicitors.com', 'images.pexels.com'],
-    },
+        domains: ['job-post-test.herokuapp.com', 'slcsolicitors.com', 'images.pexels.com']
+    }
 }
 
 
-module.exports = nextConfig
+module.exports = {...nextConfig, ...pwaConfig}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head><title>Job Portal</title></Head>
+            <Head>
+                <title>Job Finder</title>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png" />
+                <meta name="theme-color" content="#fff" />
+            </Head>
             <body className={"bg-indigo-50"}>
             <Main />
             <NextScript />
