ee5aec5f335ef02e62713065bf06325d58498559
Safal Sharma <safal.sharma.ss@gmail.com>
2022-07-08 08:01:11 +0545

    disable pwa in development.

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const pwaConfig = withPWA({
     pwa: {
         dest: "public",
         register: true,
-        skipWaiting: true
+        skipWaiting: true,
+        disable:process.env.NODE_ENV === 'development'
     }
 })
 
